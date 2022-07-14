d76499b1786a3aa64fdfceb17601d70394346803
Serrin Doscher <serrin.doscher53@myhunter.cuny.edu>
2022-07-14 16:41:05 -0400

    added grid display and styling

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This Project is being made by following a YouTube tutorial from content creator The Net Ninja
 
 Link to chanel: https://www.youtube.com/c/TheNetNinja
+
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import useWordle from '../hooks/useWordle'
+import Grid from './Grid'
 
 export default function Wordle({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution)
@@ -14,11 +15,13 @@ export default function Wordle({ solution }) {
   useEffect(() => {
     console.log(guesses, turn, isCorrect)
   }, [guesses, turn, isCorrect])
-  
+
   return (
     <div>
         <div>solution - {solution}</div>
         <div>Current Guess - {currentGuess}</div>
+
+        <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
     </div>
   )
 }
